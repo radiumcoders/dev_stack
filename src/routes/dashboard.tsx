2c861cc021da212377adae9340bf190902d500a9
radium coders <radiumcoders@gmail.com>
2026-03-28 23:33:29 +0530

    infinite redirect profile loop fixed :) Fix infinite redirect loop and update UI accents
    Use getProfile to check for existing profiles and add a server-side
    getSession
    loader to fetch session headers, preventing redirect cycles during
    profile
    creation. Also tweak hero color classes, root min-height, and add an
    animated
    dashboard link.

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,5 +1,6 @@
 import { authMiddleware } from '#/lib/middleware'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
+import { motion } from 'motion/react'
 
 export const Route = createFileRoute('/dashboard')({
   component: RouteComponent,
@@ -9,5 +10,61 @@ export const Route = createFileRoute('/dashboard')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/dashboard"!</div>
+  const MotionLink = motion.create(Link)
+  const item = { href: '/dashboard', label: 'Dashboard' }
+  return (
+    <div>
+      <MotionLink
+        key={item.href}
+        to={item.href}
+        className="group p-2 h-full w-16 md:w-20 flex items-center justify-center relative overflow-hidden shrink-0"
+        initial="initial"
+        whileHover="hover"
+      >
+        {/* TEXT */}
+        <motion.span
+          variants={{
+            initial: { y: 0 },
+            hover: { y: -2 },
+          }}
+          transition={{ type: 'spring', stiffness: 400, damping: 20 }}
+          className="relative z-10 text-foreground group-hover:text-primary-foreground"
+        >
+          {item.label}
+        </motion.span>
+
+        {/* BACKGROUND */}
+        <motion.div
+          variants={{
+            initial: {
+              y: '120%',
+              scale: 0.8,
+              rotate: 8,
+            },
+            hover: {
+              y: '0%',
+              scale: 1,
+              rotate: 0,
+            },
+          }}
+          transition={{
+            type: 'spring',
+            stiffness: 260,
+            damping: 20,
+          }}
+          className="absolute inset-0 bg-primary rounded-md"
+        />
+
+        {/* OPTIONAL GLOW */}
+        <motion.div
+          variants={{
+            initial: { opacity: 0 },
+            hover: { opacity: 0.15 },
+          }}
+          transition={{ duration: 0.3 }}
+          className="absolute inset-0 bg-white blur-xl"
+        />
+      </MotionLink>
+    </div>
+  )
 }
